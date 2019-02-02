582f3985651dc153755391e971030ac14bb7faf2
Tiv Robo <tivrobo@gmail.com>
2019-02-02 23:36:42 +0200

    xx

--- octoprint_LCD1604/__init__.py
+++ octoprint_LCD1604/__init__.py
@@ -34,37 +34,57 @@ class OctoPrintLcd1604(octoprint.plugin.StartupPlugin,
         self.lcd.create_char(1, self.block)
 
     def on_after_startup(self):
-        self._logger.info("plugin initialized !")
+        self._logger.info("plugin initialized!")
 
     def on_print_progress(self, storage, path, progress):
         lcd = self.lcd
         cols = self.cols
         rows = self.rows
 
+        self._logger.info("cols: " + str(cols))
+        self._logger.info("rows: " + str(rows))
+
         # percent completed
         str_progress = str(str(progress)+'%')
+        
+        self._logger.info("str_progress: " + str_progress)
+        self._logger.info("len(str_progress): " + str(len(str_progress)))
+        self._logger.info("int(cols - len(str_progress)): " + str(int(cols - len(str_progress))))
+
         lcd.cursor_pos = (0, 0)
         lcd.write_string('Completed:')
+
         lcd.cursor_pos = (0, int(cols - len(str_progress)))
         lcd.write_string(str_progress)
 
         # estimated time
         lcd.cursor_pos = (1, 0)
         lcd.write_string('ETA:')
+
         if progress == 1:
             self.start_date = time.time()
+
         if progress > 5 and progress < 100:
             now = time.time()
             elapsed = now - self.start_date
             average = elapsed / (progress - 1)
             remaining = int((100 - progress) * average)
             remaining = str(datetime.timedelta(seconds=remaining))
+            
+            self._logger.info("remaining: " + remaining)
+            self._logger.info("len(remaining): " + str(len(remaining)))
+            self._logger.info("int(cols - len(remaining)): " + str(int(cols - len(remaining))))
+            
             lcd.cursor_pos = (1, int(cols - len(remaining)))
             lcd.write_string(remaining)
 
         # progress bar
         percent = int(progress/5) + 1
         completed = '\x01' * percent
+
+        self._logger.info("percent: " + str(percent))
+        self._logger.info("completed: " + str(completed))
+
         lcd.cursor_pos = (3, 0)
         lcd.write_string(completed)
 
